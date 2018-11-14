f8ae146eae11297cf80ab214b47a9dfe2cefbed3
chrisblaer <cblaer@gmail.com>
2018-11-14 11:27:47 +1100

    initial logic for server sent events

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,15 @@
 class ListingsController < ApplicationController
+  include ActionController::Live
+
   def index
-    @listings = Listing.all
+    @listings = Listing.all  
+  end
+
+  def price_response
+    response.headers['Content-Type'] = 'text/event-stream'
+    sse = SSE.new(response.stream, retry: 300, event: "ping")
+    sse.write({ name: 'John'}, id: 5)
+  ensure
+    sse.close
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Listing.destroy_all
+l1 = Listing.new
+l1.start_time = Time.now
+l1.start_price = 100
+l1.end_price = 100
+l1.save
+l2 = Listing.new
+l2.start_time = Time.now + 300
+l2.start_price = 100
+l2.end_price = 100
+l2.save
+l3 = Listing.new
+l3.start_time = Time.now + 3000
+l3.start_price = 100
+l3.end_price = 100
+l3.save
