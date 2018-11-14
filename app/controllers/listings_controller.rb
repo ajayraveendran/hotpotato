766e5d9e0a24658bd1063a6788ceaf8cbf70dc4b
chrisblaer <cblaer@gmail.com>
2018-11-14 15:41:07 +1100

    sse listing end price to index page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,14 +2,26 @@ class ListingsController < ApplicationController
   include ActionController::Live
 
   def index
-    @listings = Listing.all  
+    @listings = Listing.all
   end
 
   def price_response
     response.headers['Content-Type'] = 'text/event-stream'
-    sse = SSE.new(response.stream, retry: 300, event: "ping")
-    sse.write({ name: 'John'}, id: 5)
+    sse = SSE.new(response.stream, retry: 1000, event: "ping")
+
+    result = {}
+    listings = Listing.all
+    listings.each do |listing|
+      time_diff = (Time.now.to_i - listing.start_time.to_i)
+      new_price = listing.start_price * time_diff
+      result[listing.id] = new_price
+    end
+    
+    sse.write({ listings: result})
   ensure
     sse.close
+  end
+
+  def update_price
   end
 end
