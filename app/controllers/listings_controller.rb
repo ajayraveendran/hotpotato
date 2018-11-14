d65862aeac9d33414c6b65efdf993085ac0dcc09
AjayRaveendran <ajay.raveendran@gmail.com>
2018-11-15 09:06:33 +1100

    added logic behind decrementing prices uniformly

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -44,13 +44,34 @@ class ListingsController < ApplicationController
 
   def price_response
     response.headers['Content-Type'] = 'text/event-stream'
-    sse = SSE.new(response.stream, retry: 1000, event: "ping")
+    sse = SSE.new(response.stream, retry: 3000, event: "ping")
+    value_time = {
+      50 => 120,
+      100 => 300,
+      500 => 600,
+      1000 => 900,
+      2500 => 1200,
+      5000 => 1500
+    }
+    def time_grab obj, amt
+      obj.each do |key, value|
+        if amt <= key
+          return value
+          break
+        end 
+      end 
+    end
 
+    
     result = {}
     listings = Listing.all
     listings.each do |listing|
-      time_diff = (Time.now.to_i - listing.start_time.to_i)
-      new_price = listing.start_price * time_diff
+      total_seconds = time_grab(value_time, listing.start_price)
+      interval = 3
+      time_diff = (Time.now - listing.start_time)
+      no_of_decrements = total_seconds / interval
+      dollar_decrements = listing.start_price / no_of_decrements
+      new_price = (listing.start_price - ((time_diff * dollar_decrements) / interval)).round(1)
       result[listing.id] = new_price
     end
     
