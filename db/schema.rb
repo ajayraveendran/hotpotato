263c0383f8dd96d9adc6b05ad39040e1a915c016
andrewgodman <andrew@godman.io>
2018-11-15 12:24:02 +1100

    added is_active value for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_090348) do
+ActiveRecord::Schema.define(version: 2018_11_15_010824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_090348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "is_active"
   end
 
 end
