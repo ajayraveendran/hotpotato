9096f55772e6075805e9e807f17098971c28c3e2
chrisblaer <cblaer@gmail.com>
2018-11-16 11:17:24 +1100

    enforce username uniqueness and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :seller_listings, class_name: "Listing", foreign_key: "seller_id"
     has_secure_password
     validates_uniqueness_of :username
+    validates :username, presence: true
 end
---
+++ db/migrate/20181115233627_enfore_uniqueness_of_username.rb
@@ -0,0 +1,6 @@
+class EnforeUniquenessOfUsername < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :users, :username, false
+    add_index :users, :username, unique: true
+  end
+end
