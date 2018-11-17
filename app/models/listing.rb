ff1560d27e54c03f61a792a426ce747103294169
AjayRaveendran <ajay.raveendran@gmail.com>
2018-11-18 02:22:54 +1100

    Added listing validations, logic around time and added  countdowns

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,11 @@
 class Listing < ApplicationRecord
-  
+  validates :start_price, presence: true
+  validates :start_time, presence: true
+  validate :start_time_cannot_be_in_the_past
+
+  def start_time_cannot_be_in_the_past
+    if start_time.present? && start_time <= Time.now.utc
+      errors.add(:start_time, "can't be in the past")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_233627) do
+ActiveRecord::Schema.define(version: 2018_11_16_024422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_233627) do
     t.string "status"
     t.integer "buyer_id"
     t.integer "seller_id"
+    t.datetime "end_time"
   end
 
   create_table "users", force: :cascade do |t|
