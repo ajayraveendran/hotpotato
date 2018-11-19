ea87e89ee8703b8de98756256f7b114954ce418d
chrisblaer <cblaer@gmail.com>
2018-11-19 11:05:16 +1100

    fix buy button

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ApplicationRecord
 
   validates :start_price, presence: true
   validates :start_time, presence: true
-  validate :start_time_cannot_be_in_the_past
+  validate :start_time_cannot_be_in_the_past, on: :create
 
   def start_time_cannot_be_in_the_past
     if start_time.present? && start_time <= Time.now.utc
